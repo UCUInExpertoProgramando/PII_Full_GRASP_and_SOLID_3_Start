75397996761e78fb4cb0069445019f5d995d3066
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-20 11:55:02 -0300

    Update FilePrinter.cs

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -11,28 +11,3 @@ namespace Full_GRASP_And_SOLID.Library
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-public void PrintRecipe(Recipe recipe, Destination destination)
-        {
-            if (destination == Destination.Console)
-            {
-                Console.WriteLine(recipe.GetTextToPrint());
-            }
-            else
-            {
-                File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
-            }
-        }
