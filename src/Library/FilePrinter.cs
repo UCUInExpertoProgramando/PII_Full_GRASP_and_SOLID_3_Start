1a3080a9d98a7602c8bad580d4a9640ddd91da68
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-20 11:46:55 -0300

    Create FilePrinter.cs

---
+++ src/Library/FilePrinter.cs
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public void PrintRecipe(Recipe recipe, Destination destination)
+        {
+            if (destination == Destination.Console)
+            {
+                Console.WriteLine(recipe.GetTextToPrint());
+            }
+            else
+            {
+                File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+            }
+        }
