c983f92bd5ed43377da95aaae85e3f151e458d14
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-20 11:52:29 -0300

    Update FilePrinter.cs

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -1,4 +1,16 @@
+using System;
+using System.IO;
 
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class FilePrinter : IPrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+                File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());   
+        }
+    }
+}
 
 
 
