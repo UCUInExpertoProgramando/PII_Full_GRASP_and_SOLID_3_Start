de53ccfc9f61dfaceacc7a07182cd2fb5b5d936d
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-23 21:13:40 -0300

    Corregido error del comentario de ConsolePrinter.cs

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -7,7 +7,7 @@ namespace Full_GRASP_And_SOLID.Library
     {
         public void PrintRecipe(Recipe recipe)
         {
-            //Al llamar a PrintRecipe desde un objeto tipo ConsolePrinter, imprimos por consola los datos de la receta,
+            //Al llamar a PrintRecipe desde un objeto tipo ConsolePrinter, imprimimos por consola los datos de la receta,
             //esto no modifica ninguna información ni genera ningún resultado inesperado, por lo que cumplimos
             //con el principio LSP.
             Console.WriteLine(recipe.GetTextToPrint());
