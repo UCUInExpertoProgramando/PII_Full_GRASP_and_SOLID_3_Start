b4bc75d84b8de8a35e4977f487df282552fbe542
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-23 21:03:37 -0300

    Agregado el comentario de explicación del patrón utilizado IPrinter.cs

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -8,6 +8,8 @@ using System.IO;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    //Creamos una interfaz, y utilizando el Polymorphism, que es parte del patrón GRASP, 
+    //hicimos un método que hace cosas diferentes dependiendo de cuál subtipo de IPrinter lo utilice.
     public interface IPrinter
     {
         public void PrintRecipe(Recipe recipe);
