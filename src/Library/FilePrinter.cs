ae89b489b7470c82265344d3bab71faa49584cbc
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-23 21:11:01 -0300

    Agregado comentario de explicación sobre FilePrinter.cs

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -7,6 +7,9 @@ namespace Full_GRASP_And_SOLID.Library
     {
         public void PrintRecipe(Recipe recipe)
         {
+                //Cuando PrintRecipe es llamado por un objeto tipo FilePrinter, escribimos el texto en
+                //un archivo, esto no modifica la información ni genera ningún resultado inesperado, por
+                //lo que cumplimos con el principio LSP
                 File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());   
         }
     }
